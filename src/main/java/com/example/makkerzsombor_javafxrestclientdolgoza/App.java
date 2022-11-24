f2c733304f2e587cbe71dcd72b7cccc643c10685
Zsombor Makker <makker.zsombor@gmail.com>
2022-11-24 09:18:19 +0100

    layout good

--- src/main/java/com/example/makkerzsombor_javafxrestclientdolgoza/App.java
+++ src/main/java/com/example/makkerzsombor_javafxrestclientdolgoza/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("list-people-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("People Table!");
         stage.setScene(scene);
         stage.show();
