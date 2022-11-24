6d67c279f5978db57dd6ceeaa26732860bbd5ead
Zsombor Makker <makker.zsombor@gmail.com>
2022-11-24 08:39:24 +0100

    listpeople layout done

---
+++ src/main/java/com/example/makkerzsombor_javafxrestclientdolgoza/ListPeopleController.java
@@ -0,0 +1,42 @@
+package com.example.makkerzsombor_javafxrestclientdolgoza;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+
+public class ListPeopleController {
+
+    @FXML
+    private Button insertButton;
+    @FXML
+    private Button updateButton;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private TableColumn nameCol;
+    @FXML
+    private TableColumn ageCol;
+    @FXML
+    private TableColumn heightCol;
+    @FXML
+    private TableColumn marriedCol;
+
+
+
+    @FXML
+    public void insertClick(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void updateClick(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void deleteClick(ActionEvent actionEvent) {
+
+    }
+}
